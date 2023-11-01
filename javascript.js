7a319c76f5ef31204fa0596aa08818f96ca8a305
Issa313 <isalee313@gmail.com>
2023-11-01 13:09:40 -0400

    added alert to button press

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
-function onClick{
-document.getElementById("output").innerHTML = "Can you see me?"
+function onClick(inputName){
+document.getElementById("output").innerHTML = "Can you see me?";
 alert('Hello, ' + inputName + '!');
+const myvariablename = {name: "Adam"};
+     console. log(myvariablename.name);
+
+     const myList = [];
+     console.log(myList[0]);
 }
+    
