831b91f8bc0d5cb48d6220613ce6f84aa8af0bb5
Issa313 <isalee313@gmail.com>
2023-11-07 12:46:53 -0500

    added alert to button click

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
 function onClick(inputName) {
-document.getElementById("output").innerHTML = "Now I am in control";
-alert('Hello, ' + inputName + '!');
+    document.getElementById("output").innerHTML = "Now i am in control";
+    alert('Hello, ' + inputName + '!');
 }
