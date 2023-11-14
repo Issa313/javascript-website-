e1ce85e1743ccd0148ba46387cc527cc5f2590d5
Issa313 <isalee313@gmail.com>
2023-11-14 13:20:50 -0500

    added console.log

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
-function onClick(inputName) {
-    document.getElementById("output").innerHTML = "TIME TO DIE!!!!!!!!!!!!!!";
-    alert('Hello, ' + inputName + '!');
+function onPress(inputValue) {
+    document.getElementById("displayedText").innerHTML = "Now I am in control";
+    const user = {name: 'Bob'};
+    console.log('Hello ' + user.name);
+
+    const names = ['Sally', 'Jerome']
+    const users = [{name: 'Sally'}, {name: 'Jerome'}]
+    console.log ('First name in list -----',names[0]);
+
+    console.log ('Second name in list of objects -----',users[1].name);
+    alert('Hello, ' + inputValue + '!');
 }
+
