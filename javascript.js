807f2406863120ebe0a4e3f38b5e8fb8367cbe07
Issa313 <isalee313@gmail.com>
2023-12-04 08:30:22 -0500

    added html

--- javascript.js
+++ javascript.js
@@ -1,13 +1,5 @@
 function onClick(inputValue) {
-    document.getElementById("displayedText").innerHTML = "JUMP SCARE!!!!!!!!!!!";
-    const user = {name: 'Bob'};
-    console.log('Hello ' + user.name);
-
-    const names = ['Sally', 'Jerome']
-    const users = [{name: 'Sally'}, {name: 'Jerome'}]
-    console.log ('First name in list -----',names[0]);
-
-    console.log ('Second name in list of objects -----',users[1].name);
+    
     const url = 'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
         if (status === 'success') {
@@ -16,7 +8,6 @@ function onClick(inputValue) {
             document.getElementById("displayedText").innerHTML = pokemonNames.join('      ');
         }
     });
-    alert('Hello, ' + inputValue + '!');
     
 }
 
