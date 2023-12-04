b3ee450379b00b3387c1756d196fae1516959530
Issa313 <isalee313@gmail.com>
2023-12-04 09:07:54 -0500

    transformed list into list of h2 tags

--- javascript.js
+++ javascript.js
@@ -3,14 +3,16 @@ function onClick(inputValue) {
     const url = 'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
         if (status === 'success') {
-            const pokemonNames = data.results.map(showNames)
+            const pokemonNames = data.results.map(pokemon => {
+               return '<h2>' + pokemon.name + '<h2>';
+            });   
             console.log(pokemonNames);
-            document.getElementById("displayedText").innerHTML = pokemonNames.join('      ');
+            document.getElementById("displayedText").innerHTML = pokemonNames;
         }
     });
-    
-}
+
+}   
 
 function showNames(pokemon) {
-    return pokemon.name 
+    return pokemon; 
 }
