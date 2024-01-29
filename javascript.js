0add7b756f7d45b3758ff4d83a3e5fe36e4def7d
Issa313 <isalee313@gmail.com>
2024-01-29 13:45:08 -0500

    added pokemon container

--- javascript.js
+++ javascript.js
@@ -1,15 +1,28 @@
-function onClick(inputValue) {
+function onClick(inputName) {
   const url = "https://pokeapi.co/api/v2/pokemon";
   $.get(url, function (data, status) {
-    if (status === "success") {
-      const pokemonNames = data.results.forEach((pokemon) => {
-        const h2Element = document.createElement("h2");
-        h2Element.innerHTML = pokemon.name;
-        console.log("Name with H2 tag ", h2Element);
-        document.getElementById("displayedText").append(h2Element);
-      });
+    if (status == "success") {
+      addNamesToHTML(data);
     } else {
-      console.log("Status was: " + status);
+      console.log("Status was " + status);
     }
   });
+}
+
+function addNamesToHTML(data) {
+  const pokemonList = data.results.map((pokemon) => {
+    const pokemonContainer = document.createElement("div");
+    pokemonContainer.setAttribute(
+      "style",
+      "width: 250px; display: inline-block;" +
+        "margin 5px; backround-color: cornflowerblue; border-radius: 30px;"
+    );
+    const title = document.createElement("button");
+    title.innerHTML = pokemon.name;
+    pokemonContainer.append(title);
+    console.log("Name with H2 tag: ", pokemonContainer);
+
+    return pokemonContainer;
+  });
+  $("#displayedText").html(pokemonList);
 }
